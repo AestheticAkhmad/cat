53666c89fec3b27c6d20975c6561d27749cb7ba2
Akhmad <akhmadzoripov@gmail.com>
2024-12-05 12:51:30 +0100

    Testing turn time

--- turn_time.py
+++ turn_time.py
@@ -4,7 +4,7 @@ from moving_forward import Robot
 robot = Robot()
 
 current_time = time.time()
-while time.time() - current_time < 0.5:
+while time.time() - current_time < 2:
     robot.turnRight()
 
 robot.stopcar()
