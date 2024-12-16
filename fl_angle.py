6afca4378c731a8fe9d8c8df6679582f00f84ede
Akhmad <akhmadzoripov@gmail.com>
2024-12-16 16:35:25 +0100

    Testing 02

--- fl_angle.py
+++ fl_angle.py
@@ -34,7 +34,7 @@ def turn(robot, angle):
         print(f"Turning Left by {abs(rad_angle):.2f} degrees")
         curr_time = time.time()
         while time.time() - curr_time < t:
-            robot.pwm.channels[robot.ENA].duty_cycle = 0x2FFF
+            robot.pwm.channels[robot.ENA].duty_cycle = 0x3FFF
             #robot.turnRight()
             robot.turnLeft()
     elif rad_angle > 0:
@@ -42,12 +42,12 @@ def turn(robot, angle):
         robot.move_speed = turn_speed
         curr_time = time.time()
         while time.time() - curr_time < t:
-            robot.pwm.channels[robot.ENB].duty_cycle = 0x2FFF
+            robot.pwm.channels[robot.ENB].duty_cycle = 0x3FFF
             #robot.turnLeft()
             robot.turnRight()
     else:
         print("No Turn Needed")
-    time.sleep(0.01)  # Adjust the sleep time for precise control
+    time.sleep(0.005)  # Adjust the sleep time for precise control
     robot.stopcar()
 
 angle_sum = 0
