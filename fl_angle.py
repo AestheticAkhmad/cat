d2cc254543242a048fd543da0b032fd2e3847734
Akhmad <akhmadzoripov@gmail.com>
2024-12-16 16:47:59 +0100

    Testing 02

--- fl_angle.py
+++ fl_angle.py
@@ -17,7 +17,7 @@ frame_width, frame_height = 640, 480
 
 # Robot parameters
 distance_to_line = 12  # Ground distance in cm, calibrated for 45-degree camera angle
-turn_speed = 0x7FFF     # Adjust speed for turning
+turn_speed = 0x6FFF     # Adjust speed for turning
 straight_speed = 0x7FFF
 
 # Turn function
