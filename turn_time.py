b25a38998df11174a66e81fd6b75296572aeab04
Akhmad <akhmadzoripov@gmail.com>
2025-01-16 11:55:25 +0100

    Trying PID

--- turn_time.py
+++ turn_time.py
@@ -3,8 +3,8 @@ from moving_forward import Robot
 
 robot = Robot()
 
-current_time = time.time()
-while time.time() - current_time < 2:
-    robot.turnRight()
+# current_time = time.time()
+# while time.time() - current_time < 2:
+#     robot.turnRight()
 
 robot.stopcar()
