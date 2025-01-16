db671c959fdfac56d830fc625608287388a64adf
Akhmad <akhmadzoripov@gmail.com>
2025-01-16 11:21:48 +0100

    Trying PID

--- controller.py
+++ controller.py
@@ -37,7 +37,8 @@ def preprocess_image(frame):
     
     # Thresholding to detect the black line
     _, binary = cv2.threshold(gray_blurred, 60, 255, cv2.THRESH_BINARY_INV)
-    _, contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
     print(contours)
     largest = max(contours, key=cv2.contourArea)
     M = cv2.moments(largest)
