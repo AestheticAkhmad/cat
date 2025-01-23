74dff863471e86173f76c9fb2e96658f76f900e0
Akhmad <akhmadzoripov@gmail.com>
2025-01-23 10:49:00 +0100

    PID new updates

--- spid.py
+++ spid.py
@@ -15,7 +15,7 @@ picam2.start()
 
 # Video settings
 frame_rate = 30
-video_duration = 20
+video_duration = 15
 frame_width, frame_height = 640, 480
 
 # # Robot parameters
@@ -34,8 +34,7 @@ max_speed = 0xFFFE
 
 # pid_direction = PID(Kp=13, Ki=66, Kd=33, setpoint=frame_width // 2)
 # pid_direction = PID(Kp=264, Ki=528, Kd=33, setpoint=frame_width // 2)
-pid_direction = PID(Kp=660, Ki=1320, Kd=82.5, setpoint=frame_width // 2)
-
+pid_direction = PID(Kp=528, Ki=0, Kd=66, setpoint=frame_width // 2)
 
 pid_direction.output_limits = (-max_speed // 2, max_speed // 2) 
 
