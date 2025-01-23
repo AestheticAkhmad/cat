5caffd916e959e19699015ed8b092abf45752f60
Akhmad <akhmadzoripov@gmail.com>
2025-01-23 11:26:23 +0100

    Fixing duck line follow

--- duck.py
+++ duck.py
@@ -34,7 +34,7 @@ straight_speed = 0x6FFF
 max_speed = 0x7FFF
 
 # pid_direction = PID(Kp=13, Ki=66, Kd=33, setpoint=frame_width // 2)
-pid_direction = PID(Kp=264, Ki=528, Kd=33, setpoint=frame_width // 2)
+pid_direction = PID(Kp=200, Ki=440, Kd=18, setpoint=frame_width // 2)
 
 pid_direction.output_limits = (-max_speed // 2, max_speed // 2) 
 
