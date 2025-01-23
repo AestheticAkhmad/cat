0ae9b41351885136540582dc59dd6aeccf6a4af1
Akhmad <akhmadzoripov@gmail.com>
2025-01-23 10:50:05 +0100

    PID new updates

--- spid.py
+++ spid.py
@@ -34,7 +34,7 @@ max_speed = 0xFFFE
 
 # pid_direction = PID(Kp=13, Ki=66, Kd=33, setpoint=frame_width // 2)
 # pid_direction = PID(Kp=264, Ki=528, Kd=33, setpoint=frame_width // 2)
-pid_direction = PID(Kp=528, Ki=0, Kd=66, setpoint=frame_width // 2)
+pid_direction = PID(Kp=0, Ki=1058, Kd=66, setpoint=frame_width // 2)
 
 pid_direction.output_limits = (-max_speed // 2, max_speed // 2) 
 
