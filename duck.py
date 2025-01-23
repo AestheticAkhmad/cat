e071b52ded439b3c304de1ca620b976e878de752
Akhmad <akhmadzoripov@gmail.com>
2025-01-23 11:14:26 +0100

    Duck avoidance

--- duck.py
+++ duck.py
@@ -28,9 +28,9 @@ frame_width, frame_height = 640, 480
 
 ### Working values
 # Robot parameters
-turn_speed = 0x7FFF
-straight_speed = 0xFFFA
-max_speed = 0xFFFE
+turn_speed = 0x5FFF
+straight_speed = 0x6FFF
+max_speed = 0x7FFF
 
 # pid_direction = PID(Kp=13, Ki=66, Kd=33, setpoint=frame_width // 2)
 pid_direction = PID(Kp=264, Ki=528, Kd=33, setpoint=frame_width // 2)
