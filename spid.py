87504ae5f5147108579dd1147e2be935878dfe1b
Akhmad <akhmadzoripov@gmail.com>
2025-01-23 10:34:07 +0100

    PID new updates

--- spid.py
+++ spid.py
@@ -29,7 +29,7 @@ frame_width, frame_height = 640, 480
 ### Working values
 # Robot parameters
 turn_speed = 0x7FFF
-straight_speed = 0xFFFA
+straight_speed = 0xFFF0
 max_speed = 0xFFFE
 
 # pid_direction = PID(Kp=13, Ki=66, Kd=33, setpoint=frame_width // 2)
