26c433862950293d5e1d70c91df8d06c8174f16b
Akhmad <akhmadzoripov@gmail.com>
2025-01-29 12:09:15 +0100

    Checking turning time 360

--- turn_time.py
+++ turn_time.py
@@ -2,7 +2,7 @@ import time
 from classes import Robot
 
 robot = Robot()
-turn_speed = 0x5FFF
+turn_speed = 0x6FFF
 robot.changespeed(turn_speed, turn_speed)
 current_time = time.time()
 print("Before: ", time.time())
