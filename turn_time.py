ed397e0307f0fac4da31f6a32983a95c22c790ed
Akhmad <akhmadzoripov@gmail.com>
2025-01-29 12:09:41 +0100

    Checking turning time 360

--- turn_time.py
+++ turn_time.py
@@ -6,7 +6,7 @@ turn_speed = 0x6FFF
 robot.changespeed(turn_speed, turn_speed)
 current_time = time.time()
 print("Before: ", time.time())
-while time.time() - current_time <= 2:
+while time.time() - current_time <= 2.5:
     robot.turnRight()
 print("After: ", time.time())
 
